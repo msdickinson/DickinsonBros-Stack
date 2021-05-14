8488b9ce4ffe64f0491c7af3009d955692a89e2c
Mark Dickinson <marksamdickinson@gmail.com>
2021-05-14 08:40:46 -0700

    update runners

--- Runner/DickinsonBros.Infrastructure.AzureTables.Runner.AspDI/Program.cs
+++ Runner/DickinsonBros.Infrastructure.AzureTables.Runner.AspDI/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using BaseRunner;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace DickinsonBros.Infrastructure.AzureTables.Runner.AspDI
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main()
         {
-            Console.WriteLine("Hello World!");
+            await new Program().DoMain();
+        }
+        async Task DoMain()
+        {
+            try
+            {
+                var serviceCollection = ConfigureServices();
+
+                using var provider = serviceCollection.BuildServiceProvider();
+                //var xService = provider.GetRequiredService<>();
+                var hostApplicationLifetime = provider.GetService<IHostApplicationLifetime>();
+
+                provider.ConfigureAwait(true);
+                await Task.CompletedTask;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private IServiceCollection ConfigureServices()
+        {
+            var configruation = BaseRunnerSetup.FetchConfiguration();
+            var serviceCollection = BaseRunnerSetup.InitializeDependencyInjection(configruation);
+
+            return serviceCollection;
         }
     }
 }
